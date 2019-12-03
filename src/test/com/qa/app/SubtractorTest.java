283577bc5d4ea32a9eae75bf191c9b2281286f8f
tvaidotas <tadas.vaidotas@gmail.com>
2019-12-03 14:37:30 +0000

    fixing review comment

--- src/test/com/qa/app/SubtractorTest.java
+++ src/test/com/qa/app/SubtractorTest.java
@@ -7,10 +7,14 @@ import static org.junit.Assert.assertTrue;
 
 public class SubtractorTest {
 
+    private int numberOne = 4;
+    private int numberTwo = 2;
+    private String message = "the result is 2";
+
     @Test
     public void testSubtract(){
         Subtractor subtractor = new Subtractor();
-        assertTrue(subtractor.Calculate(4, 2).equals("the result is 2"));
+        assertTrue(subtractor.Calculate(numberOne, numberTwo).equals(message));
     }
 
 }
